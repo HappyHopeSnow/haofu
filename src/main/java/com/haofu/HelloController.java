4a53176c75c76cdfa190ca3d326a6a7467d1f7b9
lianle1 <lianle1@jd.com>
2015-12-16 13:58:14 +0800

    add sign up page.

--- src/main/java/com/haofu/HelloController.java
+++ src/main/java/com/haofu/HelloController.java
@@ -8,15 +8,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
+	/**
+	 * 首页
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String index(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
 		return "index";
 	}
 
+	/**
+	 * 详情页
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.GET, value = "detail")
 	public String detail(ModelMap model) {
 		model.addAttribute("data", "detail");
 		return "detail";
 	}
+
+	/**
+	 * 登录页跳转
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "login")
+	public String login(ModelMap model) {
+		return "login";
+	}
+
+	/**
+	 * 搜索跳转
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "s")
+	public String search(ModelMap model) {
+		model.addAttribute("data", "detail");
+		return "result";
+	}
+
+
 }
---
+++ src/main/webapp/WEB-INF/pages/result.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lianle1
+  Date: 2015/12/16
+  Time: 13:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+这里显示搜索显示的结果！
+</body>
+</html>
